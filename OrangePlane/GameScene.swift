c5b0e39fce3119ac7af98f5a6c90f63e3c72ee8d
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-08-26 17:39:47 +0200

    Wall collision

--- OrangePlane/GameScene.swift
+++ OrangePlane/GameScene.swift
@@ -9,6 +9,13 @@ import Foundation
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    enum CollideType: UInt32 {
+        case Bird = 1
+        case Wall = 2
+        case Gap = 4
+    }
+    
     override func didMove(to view: SKView) {
         // Set the contactDelegate property in order to monitor collision and contact
         self.physicsWorld.contactDelegate = self
@@ -31,9 +38,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
     }
+   
     func drawPlane(){
         //
     }
+   
     func drawBackground(){
         let backgroundTexture = SKTexture(imageNamed: "back.jpeg")
         for i in 0 ... 1 {
@@ -51,7 +60,46 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             background.run(moveForever)
         }
     }
+    
     func drawObstacles(){
+        let wallPair = SKNode()
+        
+        let topTexture = SKTexture(imageNamed: "topWall.png")
+        let topWall = SKSpriteNode(texture: topTexture)
+        
+        let bottomTexture = SKSpriteNode(imageNamed: "bottomWall.png")
+        let bottomWall = SKSpriteNode(texture: bottomTexture)
+        
+        topWall.position = CGPoint(
+            x: self.frame.midX + self.frame.width,
+            y: self.frame.midY + topTexture.size().height / 2 + 200
+        )
+        
+        bottomWall.position = CGPoint(
+            x: self.frame.midX + self.frame.width,
+            y: self.frame.midY + bottomTexture.size().height / 2 - 200
+        )
+        
+        topWall.setScale(0.5)
+        bottomWall.setScale(0.5)
+        
+        topWall.physicsBody = SKPhysicsBody(rectangleOf: topTexture.size())
+        topWall.physicsBody!.contactTestBitMask = CollideType.Wall.rawValue
+        topWall.physicsBody!.categoryBitMask = CollideType.Bird.rawValue
+        topWall.physicsBody!.collisionBitMask = CollideType.Bird.rawValue
+        
+        bottomWall.physicsBody = SKPhysicsBody(rectangleOf: bottomTexture.size())
+        bottomWall.physicsBody!.contactTestBitMask = CollideType.Wall.rawValue
+        bottomWall.physicsBody!.categoryBitMask = CollideType.Bird.rawValue
+        bottomWall.physicsBody!.collisionBitMask = CollideType.Bird.rawValue
+        
+        wallPair.addChild(topWall)
+        wallPair.addChild(bottomWall)
+        
+        self.addChild(wallPair)
+    }
+    
+    @objc func fireTimer(){
         //
     }
 }
